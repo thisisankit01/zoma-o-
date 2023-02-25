2a6614f1404814c814f6cb7cc683cf728f71ddd2
Ankit Pandey <ankitechzz@gmail.com>
2023-02-26 02:21:42 +0530

    error json restaurantmenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,14 +46,21 @@ const Header = () => {
             )
           }
       <a className="hover:cursor-pointer my-auto max-sm:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg"
+        <svg 
+        xmlns="http://www.w3.org/2000/svg"
          width="24"
           height="24"
            className="text-white" 
            viewBox="0 0 24 24">
-            <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/></svg></a>
+            <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/>
+            </svg>
+            </a>
       <a className="pt-2 sm:hidden">
-        <button data-collapse-toggle="navbar-mobile" className="py-2 bg-red-500 px-4 rounded-md text-white "><svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path></svg></button></a>
+        <button data-collapse-toggle="navbar-mobile" className="py-2 bg-red-500 px-4 rounded-md text-white "><svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z">
+          </path>
+          </svg>
+        </button>
+      </a>
         </div>
     </div>
   );
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { json, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import { IMG_CDN_URL } from '../config';
 
@@ -20,32 +20,66 @@ function RestaurantMenu() {
     console.log(json.data);
     setRestaurantMenu(json.data)
    }
-   
-   console.log(restaurantMenu.menu);
+  //  console.log(restaurantMenu?.menu?.items[14413513]);
+
+   const result =[];
+
+   for(var i in restaurantMenu?.menu?.items)
+    result.push({id: i, name: restaurantMenu?.menu?.items[i].name, price: restaurantMenu?.menu?.items[i].price});
+    console.log(result);
 
   return (
-  <div>
-    <div className='grid place-content-center pt-10'>
-      <h1>Restaurant id : {resId}</h1>
-      <h2>{restaurantMenu.name}</h2>
-      <img src={IMG_CDN_URL + restaurantMenu.cloudinaryImageId} alt="" />
-      <h3>{restaurantMenu.area}</h3>
-      <h3>{restaurantMenu.city}</h3>
-      <h3>{restaurantMenu.avgRating}</h3>
-      <h3>{restaurantMenu.costForTwoMsg}</h3>
+  <>
+     <h1 className='grid place-content-center pt-6 font-bold text-4xl text-slate-500'>{restaurantMenu.name}</h1>
+    <div className='grid grid-flow-col max-sm:grid-flow-row place-content-center pt-10'>
+
+
+      <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 max-h-[450px]">
+    <a href="#">
+        <img class="p-6 rounded-t-lg" src={IMG_CDN_URL + restaurantMenu.cloudinaryImageId} alt="product image" />
+    </a>
+    <div class="px-6 pb-4 grid grid-flow-col">
+        <a href="#">
+            <h5 class="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{restaurantMenu.name}</h5>
+        </a>
+        <div class="flex items-center pt-2 mb-5">
+            <span class="bg-red-300 red-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-red-800 ml-3">{restaurantMenu.avgRating}</span>
+        </div>
+        {/* <div class="flex items-center justify-between">
+            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div> */}
     </div>
-    <div>
-      <ul>
-      {/* {Object.values(restaurantMenu.menu.items).map((item)=>(
-           <li key={item.id}>
-            {item.name}
-            </li>
-      ))} */}
-      </ul>
-    </div> 
-</div>
-  )
+    <div className='px-6 space-y-2'>
+        <h3 className='text-slate-700'><strong>Location </strong> : {restaurantMenu.area}, {restaurantMenu.city}</h3>
+        <h3 className='text-slate-700'><strong>Cost For Two </strong> : {restaurantMenu.costForTwoMsg}</h3>
+    </div>
+     </div>
+
+
+      <div className='px-14'>
+      <table className='text-center'>
+    <thead>
+      <tr>
+        <th className='border px-4 py-2 text-2xl font-bold'>Name</th>
+        <th className='border px-4 py-2 text-2xl font-bold'>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      {result.map((item) => (
+        <tr key={item.id}>
+          <td className='border px-4 py-2'>{item.name}</td>
+          <td className='border px-4 py-2'>{item.price.toFixed(2)}</td> 
+        </tr>
+      ))}
+    </tbody>
+  </table>
+      </div>
+      </div>
+</>
+  );
 }
+//toFixed returning string of 2 decimal places
 //converting objects into array of values
 
 export default RestaurantMenu
